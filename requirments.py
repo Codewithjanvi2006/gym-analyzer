b0045256736d1cb15e56767de3a7b652fc085bfd
janvi <janvi7048962683@gmail.com>
2025-08-31 20:40:35 +0530

    Update requirments.py

--- requirments.py
+++ requirments.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import datetime as dt
-import matplotlib.pyplot as plt
 from pathlib import Path
 
 st.set_page_config(page_title="Gym Weekly Muscle Work Analyzer", page_icon="💪", layout="centered")
